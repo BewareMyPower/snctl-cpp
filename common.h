b6a9e1d0ee0423c5ad18db108e9576dda9d179da
Yunze Xu <xyzinfernity@163.com>
2025-05-17 22:28:46 +0800

    feat: allow an empty token in sncloud.ini

--- common.h
+++ common.h
@@ -11,20 +11,23 @@ load_rdkafka_configs(const argparse::ArgumentParser &program) {
   if (auto rc = ini.LoadFile(config_file.c_str()); rc < 0) {
     throw std::runtime_error("Error loading config file " + config_file);
   }
-  auto get_value = [&ini](const auto &key) {
+  auto get_value = [&ini](const auto &key, bool required) {
     auto value = ini.GetValue("kafka", key, "");
-    if (strlen(value) == 0) {
+    if (strlen(value) == 0 && required) {
       throw std::runtime_error("Error: " + std::string(key) +
                                " not found in kafka section");
     }
     return std::string(value);
   };
+
   std::unordered_map<std::string, std::string> rk_conf_map{
-      {"bootstrap.servers", get_value("bootstrap.servers")},
-      {"sasl.mechanism", "PLAIN"},
-      {"security.protocol", "SASL_SSL"},
-      {"sasl.username", "user"},
-      {"sasl.password", "token:" + get_value("token")}};
+      {"bootstrap.servers", get_value("bootstrap.servers", true)}};
+  if (auto token = get_value("token", false); !token.empty()) {
+    rk_conf_map["sasl.mechanism"] = "PLAIN";
+    rk_conf_map["security.protocol"] = "SASL_SSL";
+    rk_conf_map["sasl.username"] = "user";
+    rk_conf_map["sasl.password"] = "token:" + token;
+  }
   if (auto client_id = program.present("--client-id")) {
     rk_conf_map["client.id"] = client_id.value();
   }
