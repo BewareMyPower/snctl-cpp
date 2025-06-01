BewareMyPower/snctl-cpp

commit 084b45d76c8320fd655db1a4fbf0e526b876f87f

ci: add clang-tidy check (#5)

src/main.cc
@@ -31,7 +31,7 @@
 #include "snctl-cpp/raii_helper.h"
 #include "snctl-cpp/topics.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) noexcept(false) {
   std::vector<std::string> default_config_paths{
       std::filesystem::current_path() / "sncloud.ini",
       std::filesystem::path(std::getenv("HOME")) / ".snctl-cpp" /
@@ -63,7 +63,7 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  auto rk_conf = rd_kafka_conf_new();
+  auto *rk_conf = rd_kafka_conf_new();
 
   std::array<char, 512> errstr;
   auto fail = [&errstr](const std::string &action) {
@@ -86,7 +86,9 @@ int main(int argc, char *argv[]) {
   for (auto &&[key, value] : rk_conf_map) {
     if (rd_kafka_conf_set(rk_conf, key.c_str(), value.c_str(), errstr.data(),
                           errstr.size()) != RD_KAFKA_CONF_OK) {
-      fail("set " + key + " => " + value);
+      std::string err_msg = "set " + key;
+      err_msg += " => " + value;
+      fail(err_msg);
     }
   }
 
@@ -100,7 +102,7 @@ int main(int argc, char *argv[]) {
       rd_kafka_conf_set_log_cb(
           rk_conf, +[](const rd_kafka_t *rk, int level, const char *fac,
                        const char *buf) {
-            auto file = static_cast<FILE *>(rd_kafka_opaque(rk));
+            auto *file = static_cast<FILE *>(rd_kafka_opaque(rk));
             fprintf(file, "[%d] %s: %s\n", level, fac, buf);
             fflush(file);
           });
@@ -111,14 +113,14 @@ int main(int argc, char *argv[]) {
                      const char *buf) {});
   }
 
-  auto rk =
+  auto *rk =
       rd_kafka_new(RD_KAFKA_PRODUCER, rk_conf, errstr.data(), errstr.size());
-  if (!rk) {
+  if (rk != nullptr) {
     fail("create producer");
   }
   GUARD(rk, rd_kafka_destroy);
 
-  auto rkqu = rd_kafka_queue_new(rk);
+  auto *rkqu = rd_kafka_queue_new(rk);
   GUARD(rkqu, rd_kafka_queue_destroy);
 
   try {
@@ -128,13 +130,13 @@ int main(int argc, char *argv[]) {
       configs.run();
     } else {
       if (program["--get-config"] == true) {
-        if (auto config_file = configs.config_file(); !config_file.empty()) {
-          std::cout << "config file: " << configs.config_file() << std::endl;
-          return 0;
-        } else {
+        if (const auto &config_file = configs.config_file();
+            config_file.empty()) {
           std::cerr << "Unexpected empty config file" << std::endl;
           return 2;
         }
+        std::cout << "config file: " << configs.config_file() << std::endl;
+        return 0;
       }
       std::cerr << "Invalid subcommand\n" << program << std::endl;
       return 1;

.clang-tidy
@@ -1,0 +1,67 @@
+---
+Checks: >
+  # It follows https://github.com/googleapis/google-cloud-cpp/blob/main/.clang-tidy
+  -*,
+  abseil-*,
+  bugprone-*,
+  google-*,
+  misc-*,
+  modernize-*,
+  performance-*,
+  portability-*,
+  readability-*,
+  -google-readability-braces-around-statements,
+  -google-readability-namespace-comments,
+  -google-runtime-references,
+  -misc-non-private-member-variables-in-classes,
+  -misc-const-correctness,
+  -misc-include-cleaner,
+  -modernize-return-braced-init-list,
+  -modernize-use-trailing-return-type,
+  -modernize-concat-nested-namespaces,
+  -modernize-use-nodiscard,
+  -modernize-avoid-c-arrays,
+  -modernize-type-traits,
+  -modernize-unary-static-assert,
+  -performance-move-const-arg,
+  -performance-avoid-endl,
+  -performance-enum-size,
+  -readability-braces-around-statements,
+  -readability-identifier-length,
+  -readability-magic-numbers,
+  -readability-named-parameter,
+  -readability-redundant-declaration,
+  -readability-avoid-return-with-void-value,
+  -readability-function-cognitive-complexity,
+  -bugprone-narrowing-conversions,
+  -bugprone-easily-swappable-parameters,
+  -bugprone-inc-dec-in-conditions,
+  -bugprone-implicit-widening-of-multiplication-result,
+  -bugprone-unchecked-optional-access,
+  -bugprone-unused-local-non-trivial-variable,
+  -bugprone-unused-return-value
+WarningsAsErrors: '*'
+HeaderFileExtensions:
+  - ''
+  - h
+ImplementationFileExtensions:
+  - cc
+HeaderFilterRegex: '.*'
+FormatStyle:     none
+User:            xuyunze
+CheckOptions:
+  cert-err33-c.CheckedFunctions: '^::aligned_alloc;^::asctime_s;^::at_quick_exit;^::atexit;^::bsearch;^::bsearch_s;^::btowc;^::c16rtomb;^::c32rtomb;^::calloc;^::clock;^::cnd_broadcast;^::cnd_init;^::cnd_signal;^::cnd_timedwait;^::cnd_wait;^::ctime_s;^::fclose;^::fflush;^::fgetc;^::fgetpos;^::fgets;^::fgetwc;^::fopen;^::fopen_s;^::fprintf;^::fprintf_s;^::fputc;^::fputs;^::fputwc;^::fputws;^::fread;^::freopen;^::freopen_s;^::fscanf;^::fscanf_s;^::fseek;^::fsetpos;^::ftell;^::fwprintf;^::fwprintf_s;^::fwrite;^::fwscanf;^::fwscanf_s;^::getc;^::getchar;^::getenv;^::getenv_s;^::gets_s;^::getwc;^::getwchar;^::gmtime;^::gmtime_s;^::localtime;^::localtime_s;^::malloc;^::mbrtoc16;^::mbrtoc32;^::mbsrtowcs;^::mbsrtowcs_s;^::mbstowcs;^::mbstowcs_s;^::memchr;^::mktime;^::mtx_init;^::mtx_lock;^::mtx_timedlock;^::mtx_trylock;^::mtx_unlock;^::printf_s;^::putc;^::putwc;^::raise;^::realloc;^::remove;^::rename;^::scanf;^::scanf_s;^::setlocale;^::setvbuf;^::signal;^::snprintf;^::snprintf_s;^::sprintf;^::sprintf_s;^::sscanf;^::sscanf_s;^::strchr;^::strerror_s;^::strftime;^::strpbrk;^::strrchr;^::strstr;^::strtod;^::strtof;^::strtoimax;^::strtok;^::strtok_s;^::strtol;^::strtold;^::strtoll;^::strtoul;^::strtoull;^::strtoumax;^::strxfrm;^::swprintf;^::swprintf_s;^::swscanf;^::swscanf_s;^::thrd_create;^::thrd_detach;^::thrd_join;^::thrd_sleep;^::time;^::timespec_get;^::tmpfile;^::tmpfile_s;^::tmpnam;^::tmpnam_s;^::tss_create;^::tss_get;^::tss_set;^::ungetc;^::ungetwc;^::vfprintf;^::vfprintf_s;^::vfscanf;^::vfscanf_s;^::vfwprintf;^::vfwprintf_s;^::vfwscanf;^::vfwscanf_s;^::vprintf_s;^::vscanf;^::vscanf_s;^::vsnprintf;^::vsnprintf_s;^::vsprintf;^::vsprintf_s;^::vsscanf;^::vsscanf_s;^::vswprintf;^::vswprintf_s;^::vswscanf;^::vswscanf_s;^::vwprintf_s;^::vwscanf;^::vwscanf_s;^::wcrtomb;^::wcschr;^::wcsftime;^::wcspbrk;^::wcsrchr;^::wcsrtombs;^::wcsrtombs_s;^::wcsstr;^::wcstod;^::wcstof;^::wcstoimax;^::wcstok;^::wcstok_s;^::wcstol;^::wcstold;^::wcstoll;^::wcstombs;^::wcstombs_s;^::wcstoul;^::wcstoull;^::wcstoumax;^::wcsxfrm;^::wctob;^::wctrans;^::wctype;^::wmemchr;^::wprintf_s;^::wscanf;^::wscanf_s;'
+  cppcoreguidelines-special-member-functions.AllowSoleDefaultDtor: 'true'
+  cppcoreguidelines-special-member-functions.AllowMissingMoveFunctionsWhenCopyIsDeleted: 'true'
+  performance-move-const-arg.CheckTriviallyCopyableMove: 'false'
+  readability-identifier-naming.LocalVariableCase: 'lower_case'
+  readability-identifier-naming.LocalConstantCase: 'lower_case'
+  readability-identifier-naming.StructCase: 'CamelCase'
+  readability-identifier-naming.ClassCase: 'CamelCase'
+  readability-identifier-naming.ParameterCase: 'lower_case'
+  readability-identifier-naming.MethodCase: 'lower_case'
+  readability-identifier-naming.FunctionCase: 'lower_case'
+  readability-identifier-naming.MemberCase: 'lower_case'
+  readability-identifier-naming.PrivateMemberSuffix: '_'
+  readability-identifier-naming.PublicMemberSuffix: ''
+SystemHeaders: false

.github/workflows/lint.yaml
@@ -1,0 +1,57 @@
+# Copyright 2025 Yunze Xu
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+name: Lint
+
+on:
+  push:
+    branches:
+      - main
+  pull_request:
+    branches:
+      - main
+
+concurrency:
+  group: ${{ github.workflow }}-${{ github.ref }}
+  cancel-in-progress: true
+
+jobs:
+
+  lint:
+    name: Lint check
+    runs-on: ubuntu-latest
+    timeout-minutes: 60
+
+    steps:
+      - uses: actions/checkout@v3
+        with:
+          fetch-depth: 0
+          submodules: recursive
+
+      - name: Install necessary tools
+        run: |
+          sudo apt-get install -y clang-tidy
+
+      - name: Build
+        run: |
+          cmake -B build
+          cmake --build build -j4
+
+      - name: Tidy
+        run: |
+          run-clang-tidy -p build
+          if [[ $? -ne 0 ]]; then
+            echo "clang-tidy failed"
+            exit 1
+          fi

include/snctl-cpp/configs.h
@@ -26,7 +26,7 @@
 
 struct KafkaConfigs {
   std::string bootstrap_servers = "localhost:9092";
-  std::string token = "";
+  std::string token;
 };
 
 struct LogConfigs {
@@ -39,7 +39,7 @@ struct LogConfigs {
 
 class Configs : public SubCommand {
 public:
-  Configs(argparse::ArgumentParser &parent) : SubCommand("configs") {
+  explicit Configs(argparse::ArgumentParser &parent) : SubCommand("configs") {
     update_command_.add_description("Update key-value from the INI section");
     update_command_.add_argument("--kafka-url")
         .help("The Kafka bootstrap.servers");
@@ -55,7 +55,7 @@ public:
       if (!std::filesystem::exists(file)) {
         continue;
       }
-      if (loadFile(file)) {
+      if (load_file(file)) {
         break;
       }
     }
@@ -63,7 +63,7 @@ public:
       config_file_ = std::filesystem::current_path() / "sncloud.ini";
       std::cout << "No config file found. Creating " << config_file_
                 << " with the default configs" << std::endl;
-      saveFile();
+      save_file();
     }
   }
 
@@ -98,7 +98,7 @@ public:
         }
       }
       if (updated) {
-        saveFile();
+        save_file();
         std::cout << "Updated config file " << config_file_ << std::endl;
       } else {
         std::cout << "No config updated" << std::endl;
@@ -122,16 +122,16 @@ private:
   KafkaConfigs kafka_configs_;
   LogConfigs log_configs_;
 
-  std::optional<std::string> getValue(const std::string &section,
-                                      const std::string &key) {
-    auto value = ini_.GetValue(section.c_str(), key.c_str());
+  std::optional<std::string> get_value(const std::string &section,
+                                       const std::string &key) {
+    const auto *value = ini_.GetValue(section.c_str(), key.c_str());
     if (value == nullptr) {
       return std::nullopt;
     }
     return std::optional(value);
   }
 
-  bool loadFile(const std::string &file) {
+  bool load_file(const std::string &file) {
     if (auto rc = ini_.LoadFile(file.c_str()); rc != SI_OK) {
       std::cerr << "Failed to load existing file " << file << ": " << rc
                 << std::endl;
@@ -143,20 +143,20 @@ private:
     log_configs_ = {};
 
     // load configs from the INI file
-    if (auto value = getValue("kafka", "bootstrap.servers"); value) {
+    if (auto value = get_value("kafka", "bootstrap.servers"); value) {
       kafka_configs_.bootstrap_servers = *value;
     } else {
       std::cerr << "No bootstrap.servers found in the kafka section. Use the "
                    "default value: "
                 << kafka_configs_.bootstrap_servers << std::endl;
     }
-    if (auto value = getValue("kafka", "token"); value) {
+    if (auto value = get_value("kafka", "token"); value) {
       kafka_configs_.token = *value;
     }
-    if (auto value = getValue("log", "enabled"); value) {
+    if (auto value = get_value("log", "enabled"); value) {
       log_configs_.enabled = std::string(*value) != "false";
       if (log_configs_.enabled) {
-        if (auto value = getValue("log", "path"); value) {
+        if (auto value = get_value("log", "path"); value) {
           log_configs_.path = *value;
         }
       }
@@ -166,7 +166,7 @@ private:
     return true;
   }
 
-  void saveFile() {
+  void save_file() {
     ini_.SetValue("kafka", "bootstrap.servers",
                   kafka_configs_.bootstrap_servers.c_str());
     ini_.SetValue("kafka", "token", kafka_configs_.token.c_str());

include/snctl-cpp/rk_event_wrapper.h
@@ -20,9 +20,9 @@
 
 class RdKafkaEvent final {
 public:
-  static RdKafkaEvent poll(rd_kafka_t *rk, rd_kafka_queue_t *rkqu) {
-    auto event = rd_kafka_queue_poll(rkqu, -1 /* infinite timeout */);
-    if (rd_kafka_event_error(event)) {
+  static RdKafkaEvent poll(rd_kafka_queue_t *rkqu) {
+    auto *event = rd_kafka_queue_poll(rkqu, -1 /* infinite timeout */);
+    if (rd_kafka_event_error(event) != RD_KAFKA_RESP_ERR_NO_ERROR) {
       throw std::runtime_error(rd_kafka_event_error_string(event));
     }
     return RdKafkaEvent(event);
@@ -45,5 +45,5 @@ public:
 private:
   rd_kafka_event_t *event_;
 
-  RdKafkaEvent(rd_kafka_event_t *event) : event_(event) {}
+  explicit RdKafkaEvent(rd_kafka_event_t *event) : event_(event) {}
 };

include/snctl-cpp/subcommand.h
@@ -22,9 +22,9 @@
 
 class SubCommand {
 public:
-  SubCommand(const std::string &name) : name_(name), handle_(name) {}
+  explicit SubCommand(const std::string &name) : name_(name), handle_(name) {}
 
-  bool used_by_parent(argparse::ArgumentParser &parent) const noexcept {
+  bool used_by_parent(argparse::ArgumentParser &parent) const {
     return parent.is_subcommand_used(handle_);
   }
 

include/snctl-cpp/topics.h
@@ -26,7 +26,7 @@
 
 class Topics : public SubCommand {
 public:
-  Topics(argparse::ArgumentParser &parent) : SubCommand("topics") {
+  explicit Topics(argparse::ArgumentParser &parent) : SubCommand("topics") {
     create_command_.add_description("Create a topic");
     create_command_.add_argument("topic").help("Topic to create").required();
     create_command_.add_argument("-p")

include/snctl-cpp/topics/create_topic.h
@@ -28,8 +28,8 @@
 inline void create_topic(rd_kafka_t *rk, rd_kafka_queue_t *rkqu,
                          const std::string &topic, int num_partitions) {
   std::array<char, 512> errstr;
-  auto rk_topic = rd_kafka_NewTopic_new(topic.c_str(), num_partitions, 1,
-                                        errstr.data(), errstr.size());
+  auto *rk_topic = rd_kafka_NewTopic_new(topic.c_str(), num_partitions, 1,
+                                         errstr.data(), errstr.size());
   if (rk_topic == nullptr) {
     throw std::runtime_error("Failed to create topic: " +
                              std::string(errstr.data()));
@@ -39,7 +39,7 @@ inline void create_topic(rd_kafka_t *rk, rd_kafka_queue_t *rkqu,
   rd_kafka_CreateTopics(rk, &rk_topic, 1, nullptr, rkqu);
 
   try {
-    RdKafkaEvent::poll(rk, rkqu);
+    RdKafkaEvent::poll(rkqu);
     std::cout << R"(Created topic ")" << topic << R"(" with )" << num_partitions
               << " partition" << (num_partitions == 1 ? "" : "s") << std::endl;
   } catch (const std::runtime_error &e) {

commit e59537cdf70ddbab5ff0ddc51e37ea25af712692

fix: regression by 084b45d7

src/main.cc
@@ -115,7 +115,7 @@ int main(int argc, char *argv[]) noexcept(false) {
 
   auto *rk =
       rd_kafka_new(RD_KAFKA_PRODUCER, rk_conf, errstr.data(), errstr.size());
-  if (rk != nullptr) {
+  if (rk == nullptr) {
     fail("create producer");
   }
   GUARD(rk, rd_kafka_destroy);